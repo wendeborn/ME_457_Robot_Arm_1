5bb41d3719c2d9122af1c3308729407607e7d925
whiskeytango1 <59540066+whiskeytango1@users.noreply.github.com>
2020-03-03 23:38:31 -0800

    Updated functions

--- Utilities/Serial_Arm_Mirror.ino
+++ Utilities/Serial_Arm_Mirror.ino
@@ -1,17 +1,10 @@
-/***************************
-* Arduino Sketch to control a Dynamixel robot arm
-* ME 457/557  
-* 3/3/2020
-* Drew Wendeborn
-* dw8@pdx.edu
-***************************/
 
 const uint8_t joint_num = 5;                // Number of joints in the robot arm
 const uint8_t gripper_num = 1;
 const uint16_t baudrate = 9600;
 
 uint8_t incomingByte = 0;                   // for incoming serial data
-uint16_t joints[joint_num];                 // Two bytes for each number
+uint16_t joints[joint_num];                 // Two bytes for each joint number
 uint16_t grippers[gripper_num];
 
 void setup() {
@@ -43,31 +36,26 @@ void loop() {
 
 // send robot arm joints to MATLAB
 void sendThetas(){                            
-        for(int i = 0; i < joint_num ; i++){
+       Serial.write(1);
+       for (int i = 0; i < joint_num ; i++){
+          Serial.write(highByte(joints[i]));
+          Serial.write(lowByte(joints[i]));
+       }
+}
+
+//function to drive the robot joints
+void driveJoints(){
+   for(int i = 0; i < joint_num ; i++){
           uint8_t hi = Serial.read();
           uint8_t lo = Serial.read();
           uint16_t joint = hi << 8;           // Bit shift the hi byte to the highByte position in the variable
           joint |= lo;
           joints[i] = joint;
         }
-          Serial.write(1);
-            for (int i = 0; i < joint_num ; i++){
-              Serial.write(highByte(joints[i]));
-              Serial.write(lowByte(joints[i]));
-            }
-}
-
-//function to drive the robot joints
-void driveJoints(){
 }
 
 //function to move gripper arm
 void driveGripper(){
-  
-}
-
-//function to send gripper position to MATLAB
-void sendGripper(){
   for(int i = 0; i < gripper_num ; i++){
           uint8_t hi = Serial.read();
           uint8_t lo = Serial.read();
@@ -75,6 +63,10 @@ void sendGripper(){
           gripper |= lo;
           grippers[i] = gripper;
         }
+}
+
+//function to send gripper position to MATLAB
+void sendGripper(){
           Serial.write(6);
             for (int i = 0; i < gripper_num ; i++){
               Serial.write(highByte(grippers[i]));
